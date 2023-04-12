97921e408fa8f7519c6418325eb691efb78a9f8f
Louisa <Louisa@xxx.com>
2023-04-12 22:34:26 +0800

    Day 23 - development 及 production 環境變數指令、客製錯誤訊息

--- daily-task/day23/week5-middleware/app.js
+++ daily-task/day23/week5-middleware/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const errorHandler = require("./service/errorHandler");
 const indexRouter = require("./routes/posts");
 const postsRouter = require("./routes/posts");
 const usersRouter = require("./routes/users");
@@ -21,46 +22,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
-// express 錯誤處理
-// 自己設定的 err 錯誤
-const resErrorProd = (err, res) => {
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      message: err.message,
-    });
-  } else {
-    // log 紀錄
-    console.error("出現重大錯誤", err);
-    // 送出罐頭預設訊息
-    res.status(500).json({
-      status: "error",
-      message: "系統錯誤，請恰系統管理員",
-    });
-  }
-};
-// 開發環境錯誤
-const resErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    message: err.message,
-    error: err,
-    stack: err.stack,
-  });
-};
-// 錯誤處理
-app.use(function (err, req, res, next) {
-  // dev
-  err.statusCode = err.statusCode || 500;
-  if (process.env.NODE_ENV === "dev") {
-    return resErrorDev(err, res);
-  }
-  // production
-  if (err.name === "ValidationError") {
-    err.message = "資料欄位未填寫正確，請重新輸入！";
-    err.isOperational = true;
-    return resErrorProd(err, res);
-  }
-  resErrorProd(err, res);
-});
+app.use(errorHandler); //環境變數指令切換Dev或Prod、客製錯誤訊息(要放在router下面)
 
 /* 
 在 app.js 加入  uncaughtException、unhandledRejection 處理
